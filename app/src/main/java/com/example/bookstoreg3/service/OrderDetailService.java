2f66a36f642dd57f65a2b75e9b6ad06a3ba39216
geyClown <66467307+geyClown@users.noreply.github.com>
2022-07-12 16:45:17 +0700

    order + orderDetail service
    1.0.0

--- app/src/main/java/com/example/bookstoreg3/service/OrderDetailService.java
+++ app/src/main/java/com/example/bookstoreg3/service/OrderDetailService.java
@@ -1,28 +1,103 @@
 package com.example.bookstoreg3.service;
 
 import com.example.bookstoreg3.database.GetConnection;
+import com.example.bookstoreg3.model.OrderDetail;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class OrderDetailService {
 
-    public boolean CreateOrderDetail (String orderID, String bookID) {
+    public ArrayList<OrderDetail> GetAllOrderDetail(String orderId){
+        ArrayList<OrderDetail> orderDetailArrayList = null;
+
+        try{
+            Connection conn = new GetConnection().getConn();
+            if(conn!=null){
+                String query = "SELECT * From OrderDetail WHERE OrderID = '"+orderId+"'";
+                Statement stm = conn.createStatement();
+                ResultSet rs = stm.executeQuery(query);
+                while (rs.next()){
+                    orderDetailArrayList.add(new OrderDetail(rs.getString("OrderDetailID"),
+                                                             rs.getString("OrderID"),
+                                                             rs.getString("BookID"),
+                                                             rs.getInt("Quantity"),
+                                                             rs.getFloat("TotalUnit")));
+                }
+                return orderDetailArrayList;
+            }
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void CreateOrderDetail (String orderID, String bookID, int quantity, float price) {
+        float total = price * quantity;
         OrderService orderDetail = null;
+
         try{
             Connection conn = new GetConnection().getConn();
             if (conn != null){
-                String query = "";
+                String query = "INSERT INTO OrderDetail VALUES ('"+ orderID + bookID+"', '"+orderID+"', " +
+                        " '"+bookID+"', "+quantity+", "+total+")";
                 Statement stm = conn.createStatement();
-                if (stm.executeUpdate(query) > 0) {
-                    return true;
-                }
+                stm.executeUpdate(query) ;
                 stm.close();
                 conn.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return false;
+
+    }
+
+    public void CheckDuplicateOrderDetail (String orderID, String bookID) {
+
+        OrderService orderDetail = null;
+
+        try{
+            Connection conn = new GetConnection().getConn();
+            if (conn != null){
+                String query = "UPDATE OrderDetail \n" +
+                        "Set Quantity=Quantity+1\n" +
+                        "Where OrderID ='"+orderID+"' AND BookID ='"+bookID+"'";
+                Statement stm = conn.createStatement();
+                stm.executeUpdate(query);
+                stm.close();
+                conn.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void setQuantityOrderDetail (String orderDetailID,  int quantity, float price) {
+        float total = price * quantity;
+        OrderService orderDetail = null;
+
+        try{
+            Connection conn = new GetConnection().getConn();
+            if (conn != null){
+                String query = "UPDATE OrderDetail \n" +
+                        "Set Quantity="+quantity+", TotalUnit = "+total+"\n" +
+                        "Where OrderDetailID ='"+orderDetailID+"'";
+                Statement stm = conn.createStatement();
+                stm.executeUpdate(query);
+
+
+                stm.close();
+                conn.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/example/bookstoreg3/service/OrderService.java
+++ app/src/main/java/com/example/bookstoreg3/service/OrderService.java
@@ -5,28 +5,55 @@ import com.example.bookstoreg3.model.BookModel;
 import com.example.bookstoreg3.model.Order;
 
 import java.sql.Connection;
+
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class OrderService {
 
-    public boolean Create(String userID) {
-        Order order = null;
+    public void Create(String userID) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date currentDate = new Date(); //Tue Jul 12 13:27:39 IST 2022
+
         try{
             Connection conn = new GetConnection().getConn();
             if (conn != null){
-                String query = "";
+                String query1 = "INSERT INTO [BookManager].[dbo].[Order] VALUES ('"+formatter.format(currentDate) + userID+"', NULL, GETDATE()," +
+                        " NULL, '"+userID+"', -1)";
                 Statement stm = conn.createStatement();
-                if (stm.executeUpdate(query) > 0) {
-                    return true;
-                }
+                stm.executeUpdate(query1) ;
                 stm.close();
                 conn.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return false;
+
+    }
+
+    public String CheckOrder(String userID) {
+
+        try{
+            Connection conn = new GetConnection().getConn();
+            if (conn != null){
+                String query1 = "SELECT OrderID, From [BookManager].[dbo].[Order] WHERE UserID = "+userID+" " +
+                        "AND Status =-1";
+                Statement stm = conn.createStatement();
+                ResultSet rs = stm.executeQuery(query1);
+                if (rs.next()) {
+                    return rs.getString("OrderID");
+                }
+                stm.close();
+                conn.close();
+                rs.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
